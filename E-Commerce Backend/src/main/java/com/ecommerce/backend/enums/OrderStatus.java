46bccf0599c3a11ea16870a72b5e28223775e72c
Ritesh18117 <rgriteshbyahut18117rg@gmail.com>
2024-03-04 15:44:25 +0530

    Order Tracking

---
+++ E-Commerce Backend/src/main/java/com/ecommerce/backend/enums/OrderStatus.java
@@ -0,0 +1,20 @@
+package com.ecommerce.backend.enums;
+
+public enum OrderStatus {
+    PACKED("Packed"),
+    SHIPPED("Shipped"),
+    AT_DELIVERY_CENTRE("At Delivery Centre"),
+    OUT_FOR_DELIVERY("Out for Delivery"),
+    DELIVERED("Delivered");
+
+    private final String status;
+
+    OrderStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+}
+
