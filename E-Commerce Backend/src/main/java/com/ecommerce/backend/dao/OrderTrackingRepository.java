10ddf74d3e8bf2cfe472a91dcc558b69130903ff
Main <ritesh.gupta.18117@gmail.com>
2024-04-12 14:58:58 +0530

    Minro Fix

--- E-Commerce Backend/src/main/java/com/ecommerce/backend/dao/OrderTrackingRepository.java
+++ E-Commerce Backend/src/main/java/com/ecommerce/backend/dao/OrderTrackingRepository.java
@@ -13,6 +13,6 @@ public interface OrderTrackingRepository extends CrudRepository<OrderTracking,Lo
     @Query(value = "SELECT * FROM order_tracking o WHERE o.seller_id = :sellerId AND o.status =:status ORDER BY o.created_at ASC OFFSET 5 * :minus LIMIT 5 * :count - 5 * :minus", nativeQuery = true)
     List<OrderTracking> getBySellerIdAndStatus(Long sellerId,String status ,int count,int minus);
     OrderTracking findByOrderId(Long orderId);
-    @Query(value = "SELECT * FROM order_tracking o WHERE o.status =:status ORDER BY o.created_at DESC OFFSET 5 * :minus LIMIT 5 * :count - 5 * :minus", nativeQuery = true)
+    @Query(value = "SELECT * FROM order_tracking o WHERE o.status =:status ORDER BY o.created_at ASC OFFSET 5 * :minus LIMIT 5 * :count - 5 * :minus", nativeQuery = true)
     List<OrderTracking> getByStatusPageBreak(String status, int count,int minus);
 }
